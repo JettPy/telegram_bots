9b17513227a23c5cbdf24ad8c1c9b5a8ca10a6e5
JettPy <smel_00@bk.ru>
2023-12-24 20:39:52 +0300

    Выполнен урок 2

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot.types import Message
+import dotenv
+import os
+
+dotenv.load_dotenv()
+API_TOKEN = os.getenv('API_TOKEN')
+bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler(commands=['help'])
+def help(message: Message):
+    bot.send_message(message.chat.id, "Привет! Это эхо бот")
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message: Message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
