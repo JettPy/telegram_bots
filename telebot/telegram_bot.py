94e0cd4617a9fe3c4a59a8815a5be9c940cf54f4
FILOTOs <pila200207@gmail.com>
2024-01-12 21:33:46 +0300

    hw 12.01

--- telebot/telegram_bot.py
+++ telebot/telegram_bot.py
@@ -2,6 +2,7 @@ import telebot
 from telebot.types import Message
 import dotenv
 import os
+import time
 
 dotenv.load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
@@ -9,9 +10,28 @@ API_TOKEN = os.getenv('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
 
 
+@bot.message_handler(commands=['count'])
+def count(message: Message):
+    bot.send_message(message.chat.id, '1')
+    time.sleep(5)
+
+    bot.send_message(message.chat.id, '2')
+    time.sleep(5)
+
+    bot.send_message(message.chat.id, '3')
+    time.sleep(5)
+
+    bot.send_message(message.chat.id, '4')
+    time.sleep(5)
+
+    bot.send_message(message.chat.id, '5')
+    time.sleep(5)
+
+
 @bot.message_handler(content_types=['text'])
-def echo(message: Message):
-    bot.send_message(message.chat.id, message.text)
+def greetings(message):
+    if message.text == 'привет' or message.text == 'Привет':
+        bot.send_message(message.chat.id, f"Привет,  {str(message.from_user.username)}!")
 
 
 bot.infinity_polling()
